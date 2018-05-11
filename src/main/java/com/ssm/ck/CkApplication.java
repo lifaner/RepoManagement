338bb647afb45c4567a9d992657bb9cec758a2f3
lifan <332186748@qq.com>
2018-05-11 18:00:52 +0800

    first commit

---
+++ src/main/java/com/ssm/ck/CkApplication.java
@@ -0,0 +1,12 @@
+package com.ssm.ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkApplication.class, args);
+    }
+}
